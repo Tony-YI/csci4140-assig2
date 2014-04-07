a52bf8a20789b3d79bc6886135d85f3e9d478a6e
tony-yi <455054421@qq.com>
2014-04-07 15:25:03 +0800

    test

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 		<title>CSCI4140 Assignment 2</title>
 	</head>
 
-	<body>
-		<?php echo '<p>Hello World</p>';?>
+	<body action="./test.php">
 	</body>
 </html>
---
+++ lib.php
@@ -0,0 +1,8 @@
+<?php
+/********************************
+This is a .PHP file for CSCI4140 assignment 2.
+In this file, all the php function call is inclued.
+*********************************/
+
+
+?>
