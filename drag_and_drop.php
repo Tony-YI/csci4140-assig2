ef89e1a7d4d616eb2184dafaa45bac70f0180ede
tony-yi <455054421@qq.com>
2014-04-08 00:48:59 +0800

    testing

--- drag_and_drop.php
+++ drag_and_drop.php
@@ -16,17 +16,17 @@
 
 	if($file_name)
 	{
-		$str = $str.'file_name:'.$file_name.',';
+		$str = $str.'file_name:'.$file_name;
 	}
 	if($file_size > 1000000)
 	{
 		$file_size_flag = "File size too large. Should be less than 1MB.";
-		$str = $str.'file_size_flag:'.$file_size_flag.',';
+		$str = $str.',file_size_flag:'.$file_size_flag.',';
 	}
 	if($file_type != "image/jpeg" || $file_type != "image/jpg" || $file_type != "image/gif" || $file_type != "image/png")
 	{
 		$file_size_flag = "File should be jpeg/jpg/png/gif.";
-		$str = $str.'file_type_flag:'.$file_type_flag;
+		$str = $str.',file_type_flag:'.$file_type_flag;
 	}
 
 	$str .= "}";
