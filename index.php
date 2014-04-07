e7bcaa7e26fa5d3dbece7a49087f4193ac82ec45
tony-yi <455054421@qq.com>
2014-04-07 21:48:07 +0800

    test

--- index.php
+++ index.php
@@ -18,24 +18,9 @@
 				xhr.send(data);
 			}
 
-			function drop(e)
-			{
-				e.stopPropagation();
-				e.preventDefault();
+			
 
-				var file = e.dataTransfer.files[0]; //only one file at a time?
-				var file_name = file.name;
-
-				//You still need to add something here
-				//End of adding something
-
-				var reader = new FileReader(); //file API
-				reader.onloadend = handleReaderLoadEnd; //init the reader event handlers
-				reader.readAsDataURL(file); //begin the read operation
-			}
-
-			function init()
-			{
+			
 				dropbox.addEventListener("drop", function(e){
 					e.stopPropagation();
 					e.preventDefault();
@@ -52,7 +37,7 @@
 				}, false); //drop means some file is drop into the div called dropbox
 			}
 
-			window.addEventListener("load", init, false); //load means after the whole file is received by the client
+			
 		</script>
 		<link href="./style.css" rel="stylesheet" type="text/css"/>
 		<title>CSCI4140 Assignment 2</title>
