12913cc20b7e72155b04f7a83c905be00474e3b6
tony-yi <455054421@qq.com>
2014-04-07 18:54:24 +0800

    test

--- lib.php
+++ lib.php
@@ -172,11 +172,11 @@ function clean_storage()
 /********************************************/
 function show()
 {
-	echo $_ENV{"OPENSHIFT_DATA_DIR"}; 
-	echo $_ENV{"OPENSHIFT_REPO_DIR"};
-	$result1 = `cd "$_ENV{\"OPENSHIFT_DATA_DIR\"}" && ls`;
+	$q1 = $_ENV{"OPENSHIFT_DATA_DIR"}; 
+	$q2 = $_ENV{"OPENSHIFT_REPO_DIR"};
+	$result1 = `cd "$q1" && ls`;
 	echo $result1;
-	$result2 = `cd "$_ENV{\"OPENSHIFT_REPO_DIR\"}" && ls`;
+	$result2 = `cd "$_ENV{/"OPENSHIFT_REPO_DIR/"}" && ls`;
 	echo $result2;
 }
 
