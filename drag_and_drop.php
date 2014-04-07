22846ae3c7748661fc8cee2a00db7c11274f2d15
tony-yi <455054421@qq.com>
2014-04-07 23:06:23 +0800

    testing

--- drag_and_drop.php
+++ drag_and_drop.php
@@ -1,10 +1,11 @@
 <?php
 	require "./lib.php";
-	
-	$raw_data = file_get_contents('php://input');
-	$data = base64_decode($raw_data);	//decode the result
-	$file_name = $_SERVER['HTTP_FILE_NAME'];
-	file_put_contents($file_name, $data)
+
+	//$raw_data = file_get_contents('php://input');
+	//$data = base64_decode($raw_data);	//decode the result
+	//$file_name = $_SERVER['HTTP_FILE_NAME'];
+	//file_put_contents($file_name, $data)
 	//Do your work
-	show();
+	//show();
+	echo "hehe";
 ?>
