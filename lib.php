3572967478582ef5a0fd95692727baac57e4e6a2
tony-yi <455054421@qq.com>
2014-04-07 17:23:23 +0800

    first commit

--- lib.php
+++ lib.php
@@ -4,8 +4,6 @@ This is a .PHP file for CSCI4140 assignment 2.
 In this file, all the php function call is inclued.
 *********************************/
 
-$debug = 1;
-
 /***These functions are for PHP-MYSQL part***/
 $db_host = $_ENV{'OPENSHIFT_MYSQL_DB_HOST'};
 $db_username = $_ENV{"OPENSHIFT_MYSQL_DB_USERNAME"};
@@ -18,54 +16,66 @@ function creat_db()
 	//$dbh = new PDO('mysql:host=localhost;dbname=test', $username, $password);
 	$db_source = "mysql:host=$db_host;";
 
-	global $debug;
-	if($debug == 1)
+	try
 	{
-		echo "$db_source";
+		$db_obj = new PDO($db_source, $db_username, $db_password);
+
+		$db_obj->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		//$query = $dbh->prepare(“UPDATE client SET pass=‘cool’ WHERE  user=‘tywong’”);
+		$query1 = $db_obj->prepare("CREATE DATABASE $db_name;");
+		$query1->execute();
+
+		$query2 = $db_obj->prepare("SET GLOBAL time_zone = '-5:00';");
+		$query2->execute();
 	}
-
-	$db_obj = new PDO($db_source, $db_username, $db_password);
-
-	//$query = $dbh->prepare(“UPDATE client SET pass=‘cool’ WHERE  user=‘tywong’”);
-	$query1 = $db_obj->prepare("CREATE DATABASE $db_name;");
-	$query1->execute();
-
-	$query2 = $db_obj->prepare("SET GLOBAL time_zone = '-5:00';");
-	$query2->execute();
+	catch(PDOException $e)
+	{
+		echo "Someting went wrong when creating the database...";
+	}
 	$db_obj = NULL;
 }
 
 function drop_db()
 {
-	echo "1";
-
 	global $db_host, $db_name, $db_username, $db_password;
 	$db_source = "mysql:host=$db_host;";
-	
-	echo "2";
 
 	try
 	{
-		echo "3";
-
 		$db_obj = new PDO($db_source, $db_username, $db_password);
 
-		echo "4";
 		$db_obj->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		echo "5";
 		$query = $db_obj->prepare("DROP DATABASE $db_name;");
 		$query->execute();
-
-		echo "6";
 	}
-
 	catch(PDOException $e)
 	{
-		echo "haha";
+		echo "Someting went wrong when dropping the database...";
 	}
 
-	echo "7";
+	$db_obj = NULL;
+}
+
+function creat_table()
+{
+	global $db_host, $db_name, $db_username, $db_password;
+	$db_source = "mysql:host=$db_host;dbname=$db_name";
+
+	try
+	{
+		$db_obj = new PDO($db_source, $db_username, $db_password);
+		
+		$db_obj->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$query = $db_obj->prepare("CREATE TABLE file (file_name CHAR(255), file_size INT, upload_time TIMESTAMP, img_description CHAR(255), img_path CHAR(255), shortcut_path CHAR(255), PRIMARY KEY(file_name);");
+		$query->execute();
+	}
+	catch(PDOException $e)
+	{
+		echo "Someting went wrong when creating the...";
+	}
 	$db_obj = NULL;
 }
 
@@ -73,10 +83,20 @@ function db_execute($query)
 {
 	global $db_host, $db_name, $db_username, $db_password;
 	$db_source = "mysql:host=$db_host;dbname=$db_name";
-	$db_obj = new PDO($db_source, $db_username, $db_password);
-	
-	$q = $db_obj->prepare($query);
-	$q->execute();
+
+	try
+	{
+		$db_obj = new PDO($db_source, $db_username, $db_password);
+		
+		$db_obj->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$q = $db_obj->prepare($query);
+		$q->execute();
+	}
+	catch(PDOException $e)
+	{
+		echo "Someting went wrong when executing the query...";
+	}
 	$db_obj = NULL;
 }
 /********************************************/
--- reinit.php
+++ reinit.php
@@ -4,7 +4,13 @@ This is a .PHP file for CSCI4140 assignment 2.
 This is a reinitiate script.
 *********************************/
 require "./lib.php";
-echo "<h1>reinitiating...</h1>";
+echo "<h1>Dropping DataBase...";
 drop_db();
-echo "<h1>Done</h1>";
+echo "Done</h1>";
+echo "<h1>Creating DataBase...";
+create_db();
+echo "Done</h1>";
+echo "<h1>Creating Table...";
+create_table();
+echo "Done</h1>";
 ?>
