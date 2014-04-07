991cdbbab5f9beb4e9b45cc35641de41de9bff4e
tony-yi <455054421@qq.com>
2014-04-07 22:38:12 +0800

    testing

--- index.php
+++ index.php
@@ -1,44 +1,7 @@
 <html>
 	<head>
 		<!--drag_and_drop upload-->
-		<script language="javascript" type="text/javascript">
-			function handleReaderLoadEnd(e)
-			{
-				var data = e.target.result.split(',')[1]; //get the image data
-
-				var xhr = new XMLHttpRequest();
-				var url = "./drag_and_drop.php";  //This is the file due with the drag_and_drop upload
-				xhr.open('POST', url, true);
-
-				//You still need to add something here
-				xhr.setRequestHeader('FILE_NAME', file_name);
-				xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-				//End of adding something
-
-				xhr.send(data);
-			}
-
-			
-
-			
-				dropbox.addEventListener("drop", function(e){
-					e.stopPropagation();
-					e.preventDefault();
-
-					var file = e.dataTransfer.files[0]; //only one file at a time?
-					var file_name = file.name;
-
-					//You still need to add something here
-					//End of adding something
-
-					var reader = new FileReader(); //file API
-					reader.onloadend = handleReaderLoadEnd; //init the reader event handlers
-					reader.readAsDataURL(file); //begin the read operation
-				}, false); //drop means some file is drop into the div called dropbox
-			}
-
-			
-		</script>
+		<script language="javascript" type="text/javascript" src="./javascript/drag_and_drop.js"></script>
 		<link href="./style.css" rel="stylesheet" type="text/css"/>
 		<title>CSCI4140 Assignment 2</title>
 	</head>
---
+++ javascript/drag_and_drop.js
@@ -0,0 +1,44 @@
+function drop(e)
+{
+	e.stopPropagation();
+	e.preventDefault();
+
+	var file = e.dataTransfer.files[0]; //only one file at a time?
+	var file_name = file.name;
+
+	//You still need to add something here
+	//End of adding something
+
+	var reader = new FileReader(); //file API
+	reader.onloadend = handleReaderLoadEnd; //init the reader event handlers
+	reader.readAsDataURL(file); //begin the read operation
+}
+
+function handleReaderLoadEnd(e)
+{
+	var data = e.target.result.split(',')[1]; //get the image data
+
+	var xhr = new XMLHttpRequest();
+	var url = "./drag_and_drop.php";  //This is the file due with the drag_and_drop upload
+	xhr.open('POST', url, true);	//true means async.
+
+	//You still need to add something here
+	xhr.setRequestHeader('FILE_NAME', file_name);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	//End of adding something
+
+	xhr.send(data);
+}
+
+function dragOver(e)
+{
+	e.preventDefault();
+	e.stopPropagation();
+	e.target.style.border = "solid 5px";
+}
+function dragLeave(e)
+{
+	e.preventDefault();
+	e.stopPropagation();
+	e.target.style.border = "solid 1px";
+}
