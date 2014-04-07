ddd4abdb2b117774a4fb8c18c38788c855a6755c
tony-yi <455054421@qq.com>
2014-04-07 22:42:12 +0800

    testing

--- javascript/init.js
+++ javascript/init.js
@@ -1,8 +1,1 @@
-function init()
-{
-	document.getElementById('dropbox').addEventListener("drop", drop, false); //drop means some file is drop into the div called dropbox
-	document.getElementById('dropbox').addEventListener("dragover", dragOver, false); //dragover means there is a file over the div
-	document.getElementById('dropbox').addEventListener("dragleave", dragLeave, false); //file is leaved
-}
-
-window.addEventListener("load", init, false); //load means after the whole html file is received by the client
+document.getElementById('dropbox').addEventListener("dragover", dragOver, false);
