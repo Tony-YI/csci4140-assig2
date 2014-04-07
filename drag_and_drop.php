29bf969db0a84f7cc8301996466d8edb28a992ae
tony-yi <455054421@qq.com>
2014-04-08 00:48:21 +0800

    testing

--- drag_and_drop.php
+++ drag_and_drop.php
@@ -12,24 +12,24 @@
 	$file_size = $_SERVER{'HTTP_FILE_SIZE'};
 	$file_type = $_SERVER{'HTTP_FILE_TYPE'};
 
-	$str = "";
+	$str = "{";
 
 	if($file_name)
 	{
 		$str = $str.'file_name:'.$file_name.',';
-		echo $str;
 	}
 	if($file_size > 1000000)
 	{
 		$file_size_flag = "File size too large. Should be less than 1MB.";
-		$str .= "file_size_flag:"."$file_size_flag";
+		$str = $str.'file_size_flag:'.$file_size_flag.',';
 	}
 	if($file_type != "image/jpeg" || $file_type != "image/jpg" || $file_type != "image/gif" || $file_type != "image/png")
 	{
 		$file_size_flag = "File should be jpeg/jpg/png/gif.";
-		$str .= "file_type_flag:"."$file_type_flag";
+		$str = $str.'file_type_flag:'.$file_type_flag;
 	}
 
+	$str .= "}";
 	$response = json_encode($str);
 	echo $response;
 ?>
