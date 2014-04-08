3d29d945055e47b646a8004b3333be765daa40cf
tony-yi <455054421@qq.com>
2014-04-08 21:28:55 +0800

    testing

--- drag_and_drop.php
+++ drag_and_drop.php
@@ -31,7 +31,7 @@
 			//if($file_type != "image/jpeg" && $file_type != "image/jpg" && $file_type != "image/gif" && $file_type != "image/png")
 			//not enough since we may change the extension
 			$identity = `identify -verbose "$file_dir" | grep Format:`;
-			$type = split("/\n/", $identity)[0];
+			$type = split(" ", $identity)[0];
 			echo "identity: ".$identity;
 			echo "id: ".$id;
 			if($identity) //file type is correct
