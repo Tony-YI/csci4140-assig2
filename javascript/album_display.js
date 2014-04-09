9324b41890870b2005fe4912f7ddbe774f03f072
tony-yi <455054421@qq.com>
2014-04-09 17:50:41 +0800

    testing

--- javascript/album_display.js
+++ javascript/album_display.js
@@ -50,7 +50,82 @@ function edit_click(e)
 	e.preventDefault();
 	e.stopPropagation();
 
-	alert("EDIT");
+	console.log("EDIT is clicked.");
+
+	var parent = e.target.parentNode;
+	var img = parent.children[2].children[0];
+	var file_name = img.getAttribute('filename'); //filename is the attribute predefined	
+
+	var img_des = window.prompt("Please enter the new description here. Should be less than 50 characters.");
+
+	if(img_des == null)
+	{
+		console.log("The new image description is empty.");
+		return;
+	}
+	
+	//new image description is not empty
+	console.log("New image description is received." + img_des);
+
+	//replace the special characters
+	var final_img_des = new Array;
+	for(var i = 0; i < 50; i++)
+	{
+		final_img_des[i] = img_des[i];
+	}
+
+	final_img_des = final_img_des.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+	console.log("New description converted." + final_img_des);
+
+	//send to the server
+	var xhr = new XMLHttpRequset();
+
+	xhr.open('POST', './update_description.php', true); //true means AJAX, open a connection to this php file
+
+	xhr.setRequestHeader('img_des', final_img_des);
+	xhr.setRequestHeader('file_name', file_name);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); //this is required in order to pretend to be a form submittion
+
+	xhr.send(); //send the XHR to the server
+
+	//deal with the data received from server
+	xhr.onreadystatechange = function()
+	{
+		if(xhr.status == 200 && xhr.readyState == 4) //200OK and send XHR successfully
+		{
+			console.log(xhr.responseText);
+
+			try
+			{
+				var response = JSON.parse(xhr.responseText);
+			}
+			catch(e)
+			{
+				console.log(e);
+				return; //exit the function
+			}
+
+			console.log(response.file_name);
+			console.log(response.mysql_error);
+
+			if(response.file_name)
+			{
+				document.getElementById('file_name').innerHTML = 'File Name: ' + response.file_name;
+			}
+			else //clear the old data
+			{
+				document.getElementById('file_name').innerHTML = null;
+			}
+			if(response.mysql_error)
+			{
+				document.getElementById('mysql_error').innerHTML = 'MYSQL Error Flag: ' + response.mysql_error;
+			}
+			else
+			{
+				document.getElementById('mysql_error').innerHTML = null;
+			}
+		}
+	};
 }
 
 //mouse click on the edit
---
+++ update_description.php
@@ -0,0 +1,28 @@
+<?php
+	require "./lib.php";
+
+	header("Content-type: text/json");
+
+	$file_name = $_SERVER['HTTP_FILE_NAME'];
+	$img_des = $_SERVER['HTTP_IMG_DES'];
+
+	$array = array(); //xhr response array
+
+	global $data_dir, $img_dir, $shortcut_dir, $temp_dir;
+
+	if(!file_exists("$_img_dir")) //file not exist
+	{
+		//it looks like this is impossible to happen
+		$mysql_error = "File not exist.";
+	}
+	else //file exists
+	{
+		$mysql_error = modify_file_desc($file_name, $img_des);
+	}
+
+	$array['mysql_error'] = $mysql_error;
+	$array['file_name'] = "$file_name";
+
+	$response = json_encode($array);
+	echo ($response);
+?>
