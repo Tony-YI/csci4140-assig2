92360cd29a1dac83fee6df2df0a62f7f7de04897
tony-yi <455054421@qq.com>
2014-04-09 13:47:01 +0800

    testing

--- javascript/init.js
+++ javascript/init.js
@@ -6,7 +6,7 @@ function init()
 
 	//add event listener to the image slots
 	var num_of_img_slot = document.getElementsByClassName("img_slot");
-	for(var i = 0; i < num_of_img_slot; i++)
+	for(var i = 0; i < num_of_img_slot.length; i++)
 	{
 		num_of_img_slot[i].addEventListener("mouseenter", img_slot_mouse_on, false);//false means top-down.
 		num_of_img_slot[i].addEventListener("mouseleave", img_slot_mouse_off, false);
@@ -15,14 +15,14 @@ function init()
 
 	//add event listener to the edit
 	var num_of_edit = document.getElementsByClassName("edit");
-	for(var i = 0; i < num_of_edit; i++)
+	for(var i = 0; i < num_of_edit.length; i++)
 	{
 		num_of_edit[i].addEventListener("click", edit_click, false);
 	}
 
 	//add event listener to the delete
 	var num_of_delete = document.getElementsByClassName("delete");
-	for(var i = 0; i < num_of_delete; i++)
+	for(var i = 0; i < num_of_delete.length; i++)
 	{
 		num_of_delete[i].addEventListener("click", delete_click, false);
 	}
