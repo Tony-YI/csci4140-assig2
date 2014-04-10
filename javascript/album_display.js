17e54b416f3678a9f5a1e81502ac70eb71f57978
tony-yi <455054421@qq.com>
2014-04-10 23:57:49 +0800

    testing

--- javascript/album_display.js
+++ javascript/album_display.js
@@ -248,7 +248,7 @@ function img_click(e)
 	image_background.addEventListener("click", cancle_image_large, false);
 	try
 	{
-		image_background.addEventListener("onmousewheel", disable_scroll, false);
+		document.body.addEventListener("onmousewheel", disable_scroll, false);
 		console.log('heh1');
 	}
 	catch(error)
@@ -258,7 +258,7 @@ function img_click(e)
 
 	try
 	{
-		image_background.addEventListener("DOMMouseScroll", disable_scroll, false);
+		document.body.addEventListener("DOMMouseScroll", disable_scroll, false);
 		console.log('heh2');
 	}
 	catch(error)
