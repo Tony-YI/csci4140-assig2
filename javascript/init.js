4bd25a51fb0847e426e701854f88aa888f1f5fd3
tony-yi <455054421@qq.com>
2014-04-11 14:11:35 +0800

    testing

--- javascript/init.js
+++ javascript/init.js
@@ -2,6 +2,8 @@ function init()
 {
 	update_img();
 
+	setInterval(update_img());
+
 	document.getElementById('dropbox').addEventListener("drop", drop, false); //drop means some file is drop into the div called dropbox
 	document.getElementById('dropbox').addEventListener("dragover", dragOver, false); //dragover means there is a file over the div
 	document.getElementById('dropbox').addEventListener("dragleave", dragLeave, false); //file is leaved
