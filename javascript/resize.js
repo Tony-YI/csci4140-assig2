4b63c338b1c3026588ef9e14bbf331e42ee9605f
tony-yi <455054421@qq.com>
2014-04-11 18:13:52 +0800

    testing

--- javascript/resize.js
+++ javascript/resize.js
@@ -18,19 +18,19 @@ function resize(e)
 
 	console.log("direction is " + dir + " mouse_x is " + mouse_x + ", mouse_y is " + mouse_y);
 
-	var image_large = document.getElementById('image_large'); //td
+	var display_large = document.getElementById('display_large'); //div
 	var image = document.getElementById('image'); //img
 
-	//diff_x = image_large.offsetLeft - e.clientX;
-	//diff_y = image_large.offsetTop - e.clientY;
+	//diff_x = display_large.offsetLeft - e.clientX;
+	//diff_y = display_large.offsetTop - e.clientY;
 
 	o_width = image.offsetWidth;
 	o_height = image.offsetHeight;
 
-	if(dir.indexOf('n')) //n direction
+	if(dir.indexOf('n') >= 0) //n direction
 	{
-		console.log("offsetTop " + image_large.offsetTop);
-		console.log("offsetHeight " + image_large.offsetTop);
+		console.log("offsetTop " + display_large.offsetTop);
+		console.log("offsetHeight " + display_large.offsetTop);
 
 	}
 }
